f8f989477e8a182e640237a42c0a43bf23b23cb3
Kyle Gretchev <kgretchev@gmail.com>
2022-09-10 19:45:20 -0400

    Remove to_string, which shadows existing.

--- src/lib.rs
+++ src/lib.rs
@@ -43,10 +43,6 @@ impl Snowflake {
     pub fn timestamp(&self) -> i64 {
         (self.0 as i64 >> 22) + 1420070400000
     }
-
-    pub fn to_string(&self) -> String {
-        self.0.to_string()
-    }
 }
 
 impl Display for Snowflake {
