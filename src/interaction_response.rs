43ca9dbba09474503dc14fab7e410327b5f7a8c6
Kyle Gretchev <kgretchev@gmail.com>
2022-09-12 02:32:22 -0400

    Additional callback data flags

--- src/interaction_response.rs
+++ src/interaction_response.rs
@@ -46,9 +46,15 @@ pub struct InteractionCallbackData<'a> {
 }
 
 #[derive(Debug, Eq, PartialEq, Serialize_repr)]
-#[repr(u8)]
+#[repr(u16)]
 pub enum InteractionCallbackDataFlags {
-    /// Only the user that sent the interaction will see the response
-    Ephemeral = 1 << 6,
+    Crossposted = 1 << 0,
+    IsCrosspost = 1 << 1,
     SuppressEmbeds = 1 << 2,
+    SourceMessageDeleted = 1 << 3,
+    Urgent = 1 << 4,
+    HasThread = 1 << 5,
+    Ephemeral = 1 << 6,
+    Loading = 1 << 7,
+    FailedToMentionSomeRolesInThread = 1 << 8,
 }
